14835960dd53f4802a7d16e64f8f97dd53b549bf
tnkryo <pcggyro@gmail.com>
2016-05-17 17:48:19 +0900

    オブジェクト指向課題

---
+++ challenge_7/challenge7_4.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.challenge7.servlet;
+
+/**
+ *
+ * @author kobayashi
+ */
+public class challenge7_4 extends challenge7_3 {
+    public void clear(){
+        num = 0;
+        text =null;
+    }
+}
